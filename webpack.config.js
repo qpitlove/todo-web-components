d136784e2927a1a08e5cc562ced880f8a9188bf4
김정기 <kim.jeongki80@navercorp.com>
2020-04-20 19:56:20 +0900

    env: babel build target 4.2

--- webpack.config.js
+++ webpack.config.js
@@ -24,7 +24,7 @@ module.exports = {
               {
                 useBuiltIns: "usage",
                 corejs: 3,
-                targets: ["last 2 versions", "android >= 4.2"],
+                targets: ["last 2 versions", "android >= 4.4"],
               },
             ],
           ],
